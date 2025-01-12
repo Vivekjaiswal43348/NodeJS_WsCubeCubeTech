ff128e1104742aa81c16eb2cddd3c226eaeed261
Vivekjaiswal43348 <vivekjaiswal43348@gmail.com>
2025-01-12 12:25:25 +0530

    default & named export & import POC

---
+++ cartModule.js
@@ -0,0 +1,8 @@
+const addToCart = () => {
+	return "Add to cart";
+};
+
+const changeQty = () =>{
+    return 5;
+}
+module.exports = {addToCart,changeQty};
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello! GM!", 100+1000)
+const {addToCart,changeQty} = require("./cartModule");
+
+
+console.log("Hello! GM!", 100+1000);
+
+console.log('Module response:', addToCart());
+console.log('Module response:', changeQty());
